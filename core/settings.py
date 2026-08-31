5441edb5e8a8cc25b79f17beca2569a94940f189
Abdullozoda Romin <ktrc5y7vtx@privaterelay.appleid.com>
2026-08-31 22:37:13 +0500

    db fix

--- core/settings.py
+++ core/settings.py
@@ -10,21 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2!)#x+)b2wvl)2sbg0(j-f_ua)n@39i9)qovlntnvf$7%l4rj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = []
 
 
@@ -78,11 +72,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'korvon_tour',
-        'USER':'postgres',
-        'PASSWORD':'panj2007',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
@@ -138,9 +132,9 @@ MAILERS = {
             'host':'smtp.gmail.com',
             'port':587,
             'use_tls':True,
-            'username':'marufromin2007@gmail.com',
-            'password':'aezi arze hdmx iccd'
+            'username': os.getenv('EMAIL_HOST_USER'),
+            'password': os.getenv('EMAIL_HOST_PASSWORD'),
         }
     },
 }
-DEFAULT_FROM_EMAIL = 'your_mail@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
