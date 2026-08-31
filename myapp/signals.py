47f22e004bc012e9fb7fe3ffb360d5b114cff105
Abdullozoda Romin <ktrc5y7vtx@privaterelay.appleid.com>
2026-08-31 15:30:58 +0500

    signals changed

--- myapp/signals.py
+++ myapp/signals.py
@@ -3,8 +3,11 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    if kwargs.get('raw'):
+        return
     if created:
         Profile.objects.create(user=instance)
     else:
